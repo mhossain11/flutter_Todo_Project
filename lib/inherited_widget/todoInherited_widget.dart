5aa6ea5fcaf588627e852967d8e425303d2fa8d8
Faysal <munnafh11@gmail.com>
2025-06-25 01:47:54 +0600

    InheritedWidget-commend add

--- lib/inherited_widget/todoInherited_widget.dart
+++ lib/inherited_widget/todoInherited_widget.dart
@@ -3,14 +3,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:todo_app_f/shared/todoData_model.dart';
 
-class TodoInheritedWidget extends InheritedWidget{  // সকল screen এ context এর মাধ্যমে পাওয়ার জন্য করা হয়েছে।
+class TodoInheritedWidget extends InheritedWidget{ //Parents and child মত // সকল screen এ context এর মাধ্যমে পাওয়ার জন্য করা হয়েছে।
     TodoInheritedWidget( {
     super.key,
     required super.child,
     required this.dataContainer,
      required this.todoStreamController,
   }){
-     todoStreamController.sink.add(dataContainer.todos);
+     todoStreamController.sink.add(dataContainer.todos); // প্রথমে todos list Stream এর ভিতরে Data add করবে।
    }
 
   final TodoDataContainer dataContainer;
@@ -21,11 +21,11 @@ class TodoInheritedWidget extends InheritedWidget{  // সকল screen এ cont
     todoStreamController.sink.add(dataContainer.todos);
   }
 
-  static TodoInheritedWidget? maybeOf(BuildContext context){
+  static TodoInheritedWidget? maybeOf(BuildContext context){ //যদি widget tree-তে TodoInheritedWidget থাকে, তাহলে সেটাকে return করে। না থাকলে null দেয়।
     return context.dependOnInheritedWidgetOfExactType<TodoInheritedWidget>();
   }
 
-  static TodoInheritedWidget of(BuildContext context){
+  static TodoInheritedWidget of(BuildContext context){ //same কাজ, তবে assert দিয়ে check করে নিশ্চয়তা দেয় TodoInheritedWidget আছে।
     final TodoInheritedWidget? result = maybeOf(context);
     assert(result != null,'No TodoInheritedWidget found in context');
     return result!;
@@ -33,6 +33,6 @@ class TodoInheritedWidget extends InheritedWidget{  // সকল screen এ cont
 
   @override
   bool updateShouldNotify(TodoInheritedWidget oldWidget) =>
-      dataContainer != oldWidget.dataContainer;
+      dataContainer != oldWidget.dataContainer; //এটি বলে দেয়: TodoInheritedWidget যখন rebuild হয়, তখন নিচের widget গুলা notify পাবে কি না। যদি dataContainer এর reference পরিবর্তিত হয়, তাহলে notify করা হবে।
 
 }
